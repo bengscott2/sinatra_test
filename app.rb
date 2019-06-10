90c416fc27d16de4ca011ddd3704ccae27c4d7e8
Celia <celiafrom12@gmail.com>
2019-06-10 15:03:37 +0100

    added some routes

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+require 'shotgun'
 
 get '/' do
   "Hello World"
+end
+
+get '/secret' do
+  "Secret page"
+end
+
+get '/home' do
+  "Home page"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
