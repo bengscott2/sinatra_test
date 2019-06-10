8be16e29c798f6dffe0784481b0cf5b97d2b4eff
Celia <celiafrom12@gmail.com>
2019-06-10 15:51:34 +0100

    params

--- app.rb
+++ app.rb
@@ -13,7 +13,13 @@ get '/home' do
   "Home page"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name_sample = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params[:name]
+  @name_sample = params[:name]
   erb(:index)
 end
